6f1e4a47789fb886e051fdd723152c0bc767ae0e
DRFelix <56882425+DR-Felix@users.noreply.github.com>
2023-03-15 01:39:18 +0300

    Update rational_test.cpp

--- shestakov_n_a/prj.test/rational_test.cpp
+++ shestakov_n_a/prj.test/rational_test.cpp
@@ -3,13 +3,24 @@
 
 #include <rational/rational.hpp>
 
-TEST_CASE("[rational] - Rational ctor") {
-  CHECK(Rational() == Rational(0, 1));
-  CHECK(Rational(3) == Rational(3, 1));
-  CHECK(Rational(-3) == Rational(-3, 1));
-  CHECK(Rational(10, 6) == Rational(5, 3));
-  CHECK(Rational(-10, 6) == Rational(-5, 3));
-  CHECK(Rational(10, -6) == Rational(-5, 3));
-  CHECK(Rational(-10, -6) == Rational(5, 3));
-  CHECK_THROWS(Rational(1, 0));
+TEST_CASE("testing the rational functions") {
+
+    CHECK(Rational(1, 2) + Rational(3, 4) == Rational(5, 4));
+    CHECK(Rational(3, 4) - Rational(1, 2) == Rational(1, 4));
+    CHECK(Rational(3, 4) * Rational(1, 2) == Rational(3, 8));
+    CHECK(Rational(3, 4) / Rational(1, 2) == Rational(3, 2));
+
+    CHECK_THROWS(Rational(3, 4) / Rational(0, 2) == Rational(1, 4));
+   
+    CHECK((Rational(1, 2) += Rational(3, 4)) == Rational(5, 4));
+
+    CHECK(Rational(3, 4) > Rational(2, 3));
+    CHECK(Rational(10, 8) < Rational(20, 10));
+    CHECK(Rational(2, 5) >= Rational(2, 5));
+    CHECK(Rational(3, 7) <= Rational(3, 7));
+
+    CHECK(Rational(7, 9) == Rational(7, 9));
+    CHECK(Rational(25, 60) == Rational(5, 12));
+    CHECK(Rational(2, -4) == Rational(-2, 4));
+
 }
