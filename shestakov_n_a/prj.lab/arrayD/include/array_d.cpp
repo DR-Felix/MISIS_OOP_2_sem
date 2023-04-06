930dffee7b92c4c3b3c243b90dca40b710f19ea0
DRFelix <56882425+DR-Felix@users.noreply.github.com>
2023-04-06 21:25:18 +0300

    Update array_d.cpp

--- shestakov_n_a/prj.lab/arrayD/include/array_d.cpp
+++ shestakov_n_a/prj.lab/arrayD/include/array_d.cpp
@@ -1,86 +1,107 @@
 #include <algorithm>
 #include <iostream>
+#include <stdexcept>
+#include <cstdint>
 
-#include <array_d/array_d.hpp>
+#include <arrayd/arrayd.hpp>
 
 
-ArrayD::ArrayD(int s) {             //или ArrayD::ArrayD(int s) : ssize_(s) { data = new double[ssize_]; }
-    ssize_ = s;
-    data = new double[ssize_];
+ArrayD::ArrayD(const std::ptrdiff_t size) {
+    ssize_ = size;
+    if (size < 0) {
+        throw std::invalid_argument("Size must be non-negative");
+    }
+    capacity_ = ssize_ * 2;
+    data_ = new double[capacity_];
+    for (int i = 0; i < capacity_; i++) {
+        data_[i] = NULL;
+    }
 }
 
-ArrayD::ArrayD(const ArrayD& other) : ssize_(other.ssize_) //копируем уже имеющийся массив
-{
-    data = new double[ssize_];
+ArrayD::ArrayD(const ArrayD& other) {
+    ssize_ = other.ssize_;
+    capacity_ = ssize_ * 2;
+    data_ = new double[capacity_];
     for (int i = 0; i < ssize_; ++i)
-        data[i] = other.data[i];
+        data_[i] = other.data_[i];
 }
 
-ArrayD::ArrayD(std::initializer_list<double> list) : ssize_(list.size()) //создаем новый с помощью инициализирующего списка
+ArrayD::ArrayD(ArrayD&& other) noexcept //РљРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ РєРѕРїРёСЂРѕРІР°РЅРёСЏ && - РѕР·РЅР°С‡Р°РµС‚ РІСЂРµРјРµРЅРЅСѓСЋ СЃСЃС‹Р»РєСѓ, РєР°Рє С‚РѕР»СЊРєРѕ С„СѓРЅРєС†РёСЏ Р·Р°РєРѕРЅС‡РёС‚СЃСЏ, РѕР±СЉРµРєС‚ other Р±СѓРґРµС‚ СѓРґР°Р»РµРЅ
 {
-    data = new double[ssize_];
-    int i = 0;
-    for (auto& item : list)
-        data[i++] = item;
+    ssize_ = other.ssize_;
+    data_ = other.data_;
+    capacity_ = other.capacity_;
+    other.ssize_ = 0;
+    other.capacity_ = 0;
+    other.data_ = nullptr;
 }
 
 ArrayD& ArrayD::operator=(const ArrayD& other) {
-    delete[] data;
+    delete[] data_;
     ssize_ = other.ssize();
-    data = new double[ssize_];
+    capacity_ = ssize_ * 2;
+    data_ = new double[capacity_];
     for (int i = 0; i < ssize_; ++i)
-        data[i] = other.data[i];
+        data_[i] = other.data_[i];
     return *this;
 }
 
-ArrayD::ArrayD(ArrayD&& other) : ssize_(other.ssize_), data(other.data) //Конструктор копирования && означает временную ссылку,
-{                                                                      //т. е. как только компилятор выйдет из функции, объект other будет удален
-    other.ssize_ = 0;
-    other.data = nullptr;
+double& ArrayD::operator[](const std::ptrdiff_t i) {
+    if (i < 0 || i >= ssize_) {
+        throw std::out_of_range("Index out of range");
+    }
+    return data_[i];
 }
 
-double& ArrayD::operator[](int iter) const {
-    if (iter <= 0 || iter >= ssize_) { throw std::invalid_argument("invalid index of iterator"); }
-    return data[iter];
+const double& ArrayD::operator[](const std::ptrdiff_t i) const {
+    if (i < 0 || i >= ssize_) {
+        throw std::out_of_range("Index out of range");
+    }
+    return data_[i];
 }
 
-ptrdiff_t ArrayD::ssize() const { return ssize_; } //функция получения размера
+ArrayD::~ArrayD() {
+    delete[] data_;
+}
 
-void ArrayD::resize(const int& new_size) {
-    if (new_size <= 0) { throw std::invalid_argument("invalid size"); }
-    double* old = data;
+std::ptrdiff_t ArrayD::ssize() const noexcept { return ssize_; }
+
+void ArrayD::resize(const std::ptrdiff_t new_size) {
+    if (new_size <= 0) {
+        throw std::invalid_argument("New size must be non-negative");
+    }
+    if (new_size > capacity_) {
+        double* old = data_;
+        capacity_ = new_size * 2;
+        data_ = new double[capacity_];
+        for (int i = 0; i < new_size; i++) {
+            data_[i] = old[i];
+        }
+        delete[] old;
+    }
     ssize_ = new_size;
-    data = new double[new_size];
-
-    int min_size(1);
-    if (size_t(old) < new_size) {
-        min_size = size_t(old);
+    int a = ssize_;
+    for (int i = a; i < new_size; i += 1) {
+        data_[i] = NULL;
     }
-    else {
-        min_size = ssize_;
-    }
-    for (int i = 0; i < min_size; ++i) {
-        data[i] = old[i];
-    }
-    delete[] old; //обязательно следим за памятью и чистим ненужные массивы!
 }
 
-void ArrayD::insert(const int& i, const double& elem) {
-    if (i <= 0 || i > ssize_) { throw std::invalid_argument("invalid index"); }
-    ssize_ += 1;
-    double* old = data;
-    data = new double[ssize_];
-    for (int j = 0; j < i; ++j) {
-        data[j] = old[j];
+void ArrayD::insert(const std::ptrdiff_t i, const double& value) {
+    if (i < 0 || i > ssize_) {
+        throw std::out_of_range("invalid index");
     }
-    data[i] = elem;
-    for (int j = i + 1; j < ssize_; ++j) {
-        data[j] = old[j - 1];
+    (*this).resize(ssize_ + 1);
+    for (int j = ssize_ - 1; j > i; --j) {
+        data_[j] = data_[j - 1];
     }
-    delete[] old;
+    data_[i] = value;
 }
-void ArrayD::remove(const int& i) {
-    std::rotate(data + i, data + i + 1, data + ssize_);
+
+void ArrayD::remove(const std::ptrdiff_t i) {
+    if (i < 0 || i >= ssize_) { 
+        throw std::out_of_range("invalid index"); 
+    }
+    std::rotate(data_ + i, data_ + i + 1, data_ + ssize_);
     (*this).resize(ssize_ - 1);
 }
 
@@ -90,12 +111,12 @@ std::istream& ArrayD::ReadFrom(std::istream& istrm) {
         double xi = 0;
         while (!istrm.eof() && istrm.good() && i < ssize_) {
             istrm >> xi;
-            data[i] = xi;
-            i += 1;
+            data_[i] = xi;
+            i++;
         }
     }
     else {
-        throw std::exception();
+        istrm.setstate(std::ios_base::failbit);
     }
     return istrm;
 }
@@ -104,8 +125,8 @@ std::ostream& ArrayD::WriteTo(std::ostream& ostrm) {
     if (ssize_ > 0) {
         int i = 0;
         while (ostrm.good() && i < ssize_) {
-            ostrm << data[i] << " ";
-            i += 1;
+            ostrm << data_[i] << " ";
+            i++;
         }
     }
     return ostrm;
