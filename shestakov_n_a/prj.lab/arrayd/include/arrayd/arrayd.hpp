fb95a5395b62f48cad933dfc6cf77a3f12bf5830
DRFelix <56882425+DR-Felix@users.noreply.github.com>
2023-04-09 14:22:30 +0300

    Update arrayd.hpp

--- shestakov_n_a/prj.lab/arrayd/include/arrayd/arrayd.hpp
+++ shestakov_n_a/prj.lab/arrayd/include/arrayd/arrayd.hpp
@@ -1,66 +1,34 @@
-#include <iosfwd>
+#include <iostream>
+
+#pragma once
+#ifndef ARRAYD_ARRAYD_HPP_20230304
+#define ARRAYD_ARRAYD_HPP_20230304
+
+const std::exception out_of_range("Error: Out of range");
 
 class ArrayD {
-
 public:
-    /**
-     * \detail создает массив размера size, заполненого элементами со значнием по умолчанию.
-     *         если size = 0 аллокации пам€ти не происходит
-     * \param[in] size размер массива
-     * \throw std::invalid_argument если size < 0
-     */
-    explicit ArrayD(const std::ptrdiff_t size = 0);
-    ArrayD(const ArrayD& other);
-    ArrayD& operator=(const ArrayD& other);
+	ArrayD() = default;
 
-    ArrayD(ArrayD&& other) noexcept; //копирование
+	ArrayD(const ArrayD& arr);
+	explicit ArrayD(const std::ptrdiff_t size);
 
-    ~ArrayD(); //деструктор
-    /**
-     * \brief ƒоступ к элементам по индексу
-     * \param[in] i индекс возвращаемого элемента
-     * \return i-тый элемент
-     * \throw std::out_of_range если индекс вне диапазона [0, size)
-     */
-    [[nodiscard]] double& operator[](const std::ptrdiff_t i);
-    [[nodiscard]] const double& operator[](const std::ptrdiff_t i) const;
+	ArrayD& operator=(const ArrayD& rhs);
 
-    /**
-     * \return текущий размер массива
-     */
-    [[nodiscard]] std::ptrdiff_t ssize() const noexcept;
+	~ArrayD() { delete[] data_; }
 
-    /**
-     * \brief »зменение размера массива
-     * \param[in] new_size новый размер массива
-     * \throw std::invalid_argument если входной параметр new_size <= 0
-     */
-    void resize(const std::ptrdiff_t new_size);
+	double& operator[](const std::ptrdiff_t i);
+	const double& operator[](const std::ptrdiff_t i) const;
 
-    /**
-     * \brief ¬ставл€ет элемент. если размера недостаточно, делает resize
-     * \param[in] i индекс элемента, который будет добавлен
-     * \param[in] value значение, которое надо добавить
-     * \throw std::out_of_range если индекс вне диапазона [0, size]
-     */
-    void insert(const std::ptrdiff_t i, const double& value);
-
-    /**
-     * \brief ”дал€ет элемент по индексу i
-     * \param[in] i индекс удал€емого элемента
-     * \throw std::out_of_range если индекс вне диапазона [0, size)
-     */
-    void remove(const std::ptrdiff_t i);
-
-    //¬вод/вывод
-    std::istream& ReadFrom(std::istream& istrm);
-    std::ostream& WriteTo(std::ostream& ostrm);
+	std::ptrdiff_t ssize() const noexcept { return size_; };
+	void resize(const std::ptrdiff_t size);
+	void remove(std::ptrdiff_t index);
+	void insert(double val, std::ptrdiff_t index);
 
 private:
-    std::ptrdiff_t ssize_{ 0 };
-    std::ptrdiff_t capacity_{ 0 };
-    double* data_{ nullptr };
+	std::ptrdiff_t capacity_{ 0 };
+	std::ptrdiff_t size_{ 0 };
+	double* data_{ nullptr };
 };
 
-std::ostream& operator<<(std::ostream& ostrm, ArrayD& array);
-std::istream& operator>>(std::istream& istrm, ArrayD& array);
+#endif
