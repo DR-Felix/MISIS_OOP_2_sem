95fd1893ec09064f6d6e1e412a5593762de4c983
DRFelix <56882425+DR-Felix@users.noreply.github.com>
2023-04-13 17:55:44 +0300

    Update matrixs.hpp

--- shestakov_n_a/prj.lab/matrixs/include/matrixs/matrixs.hpp
+++ shestakov_n_a/prj.lab/matrixs/include/matrixs/matrixs.hpp
@@ -1,64 +1,41 @@
 #pragma once
+#ifndef MATRIXS_MATRIXS_HPP_20230317
+#define MATRIXS_MATRIXS_HPP_20230317
 
-#ifndef MATRIXS_HPP
-#define MATRIXS_HPP
-
-#include <cstdint>
-#include <iosfwd>
-#include <tuple>
+#include <sstream>
+#include <vector>
 
 const std::exception out_of_range("Error: Not valid index");
 
 class MatrixS {
 public:
     using SizeType = std::tuple<std::ptrdiff_t, std::ptrdiff_t>;
-
 public:
     explicit MatrixS(const SizeType& size = { 0, 0 });
-    MatrixS(const std::ptrdiff_t m, const std::ptrdiff_t n);
-    ~MatrixS() { delete[] data_; }
-
+    MatrixS(const std::ptrdiff_t row_count, const std::ptrdiff_t col_count);
+    ~MatrixS() {
+        delete[] data_;
+    };
+    MatrixS& operator=(const MatrixS& rhs) = default;
     MatrixS(const MatrixS& mat);
 
-    MatrixS& operator=(const MatrixS& other);
+    std::ptrdiff_t nRows() const noexcept { return n_row_; }
+    std::ptrdiff_t nCols() const noexcept { return n_col_; }
+    const SizeType& ssize() const noexcept { 
+        return SizeType {n_row_,n_col_}; 
+    };
 
-    /**
-     * \brief Возвращает элемент матрицы под индексами {i, j}
-     * \param[in] elem индексы элемента
-     * \return элемент матрицы
-     * \throw std::out_of_range если i вне диапазона [0, m) или j вне диапазона [0, n)
-     */
-    [[nodiscard]] int& at(const SizeType& elem);
-    [[nodiscard]] const int& at(const SizeType& elem) const;
-    [[nodiscard]] int& at(const std::ptrdiff_t i, const std::ptrdiff_t j);
-    [[nodiscard]] const int& at(const std::ptrdiff_t i, const std::ptrdiff_t j) const;
+    int32_t& at(const SizeType& elem);
+    const int32_t& at(const SizeType& elem) const;
+    int32_t& at(const std::ptrdiff_t i_row, const std::ptrdiff_t j_col);
+    const int32_t& at(const std::ptrdiff_t i_row, const std::ptrdiff_t j_col) const;
 
-    /**
-     * \brief Изменение размера матрицы
-     * \param[in] new_size новый размер матрицы {new_m, new_n}
-     * \throw std::invalid_argument если new_m <= 0 или new_n <= 0
-     */
     void resize(const SizeType& new_size);
-    void resize(const std::ptrdiff_t i, const std::ptrdiff_t j);
-
-    /**
-     * \return текущий размер матрицы {m, n}
-     */
-    [[nodiscard]] const SizeType& ssize() const noexcept;
-
-    /**
-     * \return количество строк в матрице (m)
-     */
-    [[nodiscard]] std::ptrdiff_t nRows() const noexcept;
-
-    /**
-     * \return количество столбцов в матрице (n)
-     */
-    [[nodiscard]] std::ptrdiff_t nCols() const noexcept;
-
+    void resize(const std::ptrdiff_t new_i, const std::ptrdiff_t new_j);
 private:
-    int* data_ = nullptr;
-    SizeType size_{ 0,0 };
+    std::ptrdiff_t n_row_{ 0 };
+    std::ptrdiff_t n_col_{ 0 };
+    int32_t* data_{ nullptr };
 };
 
 #endif
