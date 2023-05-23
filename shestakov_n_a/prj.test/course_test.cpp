218a241092c2fc354af3192175101af888858422
DRFelix <56882425+DR-Felix@users.noreply.github.com>
2023-05-23 23:36:34 +0300

    Update course_test.cpp

--- shestakov_n_a/prj.test/course_test.cpp
+++ shestakov_n_a/prj.test/course_test.cpp
@@ -1,17 +1,19 @@
-#include <iostream>
 #include <opencv2/opencv.hpp>
 #include <cmath>
-#include <cstdlib>
 
 cv::Mat niblackThreshold(const cv::Mat& src, int windowSize, double k, double& scale) {
     cv::Mat imgGray;
 
     cv::cvtColor(src, imgGray, cv::COLOR_BGR2GRAY);
 
+    // Определяем масштаб по ширине и высоте
     double scaleWidth = static_cast<double>(800) / imgGray.cols;
     double scaleHeight = static_cast<double>(600) / imgGray.rows;
+
+    // Находим меньший коэффициент масштабирования, чтобы изображение поместилось в окно 800x600
     scale = std::min(scaleWidth, scaleHeight);
 
+    // Масштабирование исходного изображения
     cv::Mat resizedSrc;
     cv::resize(imgGray, resizedSrc, cv::Size(), scale, scale);
 
@@ -21,14 +23,16 @@ cv::Mat niblackThreshold(const cv::Mat& src, int windowSize, double k, double& s
 
     for (int y = halfWindowSize; y < resizedSrc.rows - halfWindowSize; ++y) {
         for (int x = halfWindowSize; x < resizedSrc.cols - halfWindowSize; ++x) {
+            // Вычисляем среднее значение яркости в окрестности
             double mean = 0.0;
             for (int j = -halfWindowSize; j <= halfWindowSize; ++j) {
                 for (int i = -halfWindowSize; i <= halfWindowSize; ++i) {
-                    mean += resizedSrc.at<uchar>(y + j, x + i);
+                    mean += resizedSrc.at<uchar>(y + j, x + i); // возвращает значение яркости пикселя в позиции (y + j, x + i) в черно-белом изображении resizedSrc
                 }
             }
             mean /= (windowSize * windowSize);
 
+            // Вычисляем стандартное отклонение яркости в окрестности
             double stdDeviation = 0.0;
             for (int j = -halfWindowSize; j <= halfWindowSize; ++j) {
                 for (int i = -halfWindowSize; i <= halfWindowSize; ++i) {
@@ -38,8 +42,10 @@ cv::Mat niblackThreshold(const cv::Mat& src, int windowSize, double k, double& s
             }
             stdDeviation = std::sqrt(stdDeviation / (windowSize * windowSize));
 
+            // Вычисляем пороговое значение яркости для текущего пикселя
             double threshold = mean + k * stdDeviation;
 
+            // Бинаризация пикселя
             if (resizedSrc.at<uchar>(y, x) > threshold) {
                 imgThresh.at<uchar>(y, x) = 255;
             }
@@ -55,6 +61,7 @@ cv::Mat niblackThreshold(const cv::Mat& src, int windowSize, double k, double& s
 void demonstrateNiblack(const cv::Mat& src, int windowSize, double k, double& scale) {
     cv::Mat imgThresh = niblackThreshold(src, windowSize, k, scale);
 
+    // Масштабирование окна изображения
     cv::Mat resizedWindow;
     cv::resize(src, resizedWindow, cv::Size(), scale, scale);
 
@@ -63,25 +70,22 @@ void demonstrateNiblack(const cv::Mat& src, int windowSize, double k, double& sc
     cv::waitKey(0);
 }
 
-int main(int argc, char** argv) {
-    if (argc < 3) {
-        std::cout << "Ошибка: Недостаточно аргументов командной строки.\n";
-        std::cout << "Формат команды: <input_image> <window_size> <k>\n";
-        return -1;
-    }
-
-    std::string inputImagePath = argv[1];
-    int windowSize = std::atoi(argv[2]);
-    double k = std::atof(argv[3]);
-    double scale;
-
-    cv::Mat image = cv::imread(inputImagePath);
+int main() {
+    cv::Mat image = cv::imread("C:/Users/nick_/Downloads/test1.jpg");
 
     if (image.empty()) {
-        std::cout << "Ошибка: Не удалось загрузить изображение.\n";
+        std::cout << "Failed to load image." << std::endl;
         return -1;
     }
 
+    /*if (image.channels() != 1) {
+        std::cout << "Invalid image format. Only grayscale images are supported." << std::endl;
+        return -1;
+    }*/
+
+    int windowSize = 31;
+    double k = 0.2;
+    double scale;
     demonstrateNiblack(image, windowSize, k, scale);
 
     return 0;
