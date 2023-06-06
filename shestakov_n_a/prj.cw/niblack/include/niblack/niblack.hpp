e6c02f4c7e4bf9b782c7f57783a64ea917570eb7
DRFelix <56882425+DR-Felix@users.noreply.github.com>
2023-06-07 00:56:27 +0300

    Add files via upload

--- shestakov_n_a/prj.cw/niblack/include/niblack/niblack.hpp
+++ shestakov_n_a/prj.cw/niblack/include/niblack/niblack.hpp
@@ -1,4 +1,4 @@
-#ifndef NIBLACKBINARIZATION_H
+﻿#ifndef NIBLACKBINARIZATION_H
 #define NIBLACKBINARIZATION_H
 
 #include <opencv2/opencv.hpp>
@@ -10,131 +10,134 @@
 #include <fstream>
 #include <filesystem>
 
+/**
+ * @brief Namespace for std::filesystem methods
+ */
 namespace fs = std::filesystem;
 
 /**
- * @brief Класс для расчета, демонстрации и визуализации порогового значения по методу Ниблэка
+ * @brief A class for calculating, demonstrating and visualizing the threshold value using the Niblack method
  */
 class NiblackBinarization {
 public:
     /**
-    * @brief Конструктор по умолчанию для класса NiblackBinariation
+    * @brief Default constructor for the NiblackBinariation class
     */
     NiblackBinarization() = default;
 
     /**
-    * @brief Конструктор копирования для класса NiblackBinariation
-    * @param[in] cv::Mat& rhs матрица, для которой будет применен алгоритм
+    * @brief Copy constructor for the NiblackBinariation class
+    * @param[in] cv::Mat& rhs - input image
     */
     NiblackBinarization(cv::Mat& rhs);
 
     /**
-    * @brief Конструктор копирования для класса NiblackBinariation
-    * @param[in] cv::Mat& src матрица, для которой будет применен алгоритм
-    * @param[in] window_size, в котором будет происходить бинаризация
-    * @param[in] k - коэффициент, влияющий на величину порогового значения
-    * @param[in] scale - коэффициент, с которым будет масштабироваться график (по умолчанию = 1)
+    * @brief Copy constructor for the NiblackBinariation class
+    * @param[in] cv::Mat& src - input image
+    * @param[in] window_size, determining the window size for the algorithm
+    * @param[in] k - coefficient affecting the threshold value
+    * @param[in] scale - the coefficient with which the graph will be scaled (default = 1)
     */
     NiblackBinarization(cv::Mat& src, const int window_size, const double k, const double& scale);
 
     /**
-    * @brief Деструктор для класса NiblackBinariation
+    * @brief Destructor for the NiblackBinariation class
     */
     ~NiblackBinarization() = default;
 
     /**
-     * @brief Оператор доступа для класса NiblackBinarization
-     * @param[in] rhs NiblackBinarization - копируемый объект
+     * @brief Access operator for the NiblackBinarization class
+     * @param[in] rhs NiblackBinarization - copyied object
      * @return NiblackBinarization&
      */
     NiblackBinarization& operator=(const NiblackBinarization& rhs);
 
     /**
-     * @brief Метод, который проверяет корректность формата изображения
-     * @param[in] cv::Mat image - входное изображение на проверку
+     * @brief A method that checks the correctness of the image format
+     * @param[in] cv::Mat image - input image
      * @return true/false
      */
     bool check_the_image(cv::Mat image);
 
     /**
-     * @brief Метод, выполняющий бинаризацию изображения по методу Ниблэка
-     * @param[in] cv::Mat& src - входное изображение
-     * @param[in] window_size - размер области, в котором будет осуществляться бинаризация
-     * @param[in] k - коэффициент, влияющий на величину порогового значения
-     * @param[in] scale - коэффициент, с которым будет масштабироваться график (по умолчанию = 1)
-     * @return cv::Mat object после бинаризации
+     * @brief Method that performs image binarization using the Niblack method
+     * @param[in] cv::Mat& src - input image
+     * @param[in] window_size, determining the window size for the algorithm
+     * @param[in] k - coefficient affecting the threshold value
+     * @param[in] scale - the coefficient with which the graph will be scaled (default = 1)
+     * @return cv::Mat object after binarization
      */
     cv::Mat niblackThreshold(const cv::Mat& src, int window_size, double k, double& scale);
 
     /**
-     * @brief Метод, осуществляющий демонстрацию исходного и отбинаризованного изображения, полученного с помощью вызова niblackThreshold-метода
-     * @param[in] cv::Mat& src - входное изображение
-     * @param[in] window_size - размер области, в котором будет осуществляться бинаризация
-     * @param[in] k - коэффициент, влияющий на величину порогового значения
-     * @param[in] scale - коэффициент, с которым будет масштабироваться график (по умолчанию = 1)
-     * @param[in] selected_row - ряд, для которого будет осуществляться визуализация
-     * @param[in] executable_path - путь для исполняемого файла для передачи чего в plotValues-метода для визуализации
+     * @brief Method that demonstrates the source and from the binarized image obtained by calling the niblackThreshold method
+     * @param[in] cv::Mat& src - input image
+     * @param[in] window_size, determining the window size for the algorithm
+     * @param[in] k - coefficient affecting the threshold value
+     * @param[in] scale - the coefficient with which the graph will be scaled (default = 1)
+     * @param[in] selected_row - the row for which the visualization will be performed
+     * @param[in] executable_path - the path for the executable file to pass what to plotValues-method for visualization
      */
     void demonstrateNiblack(const cv::Mat& src, int window_size, double k, double scale, int selected_row, std::string executable_path);
 
     /**
-     * @brief метод, сохраняющий координаты точек в .tex-файл для визуализации
-     * @param[in] file - файл для записи координат
-     * @param[in] values - массив координат точек
-     * @param[in] color - цвет линии на графике
-     * @param[in] label - надпись в легенде графика
+     * @brief a method that stores the coordinates of points in a .tex file for visualization
+     * @param[in] file - file for recording coordinates
+     * @param[in] values - array of coordinates of points
+     * @param[in] color - line color on the chart
+     * @param[in] label - the inscription in the legend of the graph
     */
     void drawGraph(std::ofstream& file, const std::vector<double>& values, const std::string& color, const std::string& label);
 
     /**
-     * @brief Метод, в котором формируется структура .tex-файла, а также его содержимое посредством вызова drawGraph-метода
-     * @param filePath - путь к исполняему файлу, рядом с которым будет сгенерирован .tex-файл
-     * @param localIntensity - массив векторов со значениями локальной интенсивности
-     * @param meanValues - массив векторов со значениями средней яркости
-     * @param varianceValues - массив векторов со значениями среднего отклонения
-     * @param thresholdValues - массив векторов порогового значения 
-     * @param selectedRow - номер выбранной для визуализации линии
+     * @brief The method in which the structure of the .tex file is formed, as well as its contents, by calling the drawGraph method
+     * @param filePath - the path to the executable file next to which the .tex file will be generated
+     * @param localIntensity - array of vectors with local intensity values
+     * @param meanValues - array of vectors with mean brightness values
+     * @param varianceValues - array of vectors with standart deviation values
+     * @param thresholdValues - array of threshold value vectors 
+     * @param selectedRow - the number of the row selected for visualization
     */
     void plotValues(const std::string& filePath, const std::vector<double>& localIntensity, const std::vector<double>& meanValues, const std::vector<double>& varianceValues, const std::vector<double>& thresholdValues, int selectedRow);
 
     /**
-     * @brief Сеттер для window_size
-     * @param[in] window_size - размер области, в котором будет осуществляться бинаризация
+     * @brief window_size setter
+     * @param[in] window_size, determining the window size for the algorithm
     */
     void setWindowSize(int window_size) {
         window_size_ = window_size;
     }
 
     /**
-     * @brief Сеттер для k
-     * @param[in] k - коэффициент, влияющий на величину порогового значения
+     * @brief K setter
+     * @param[in] k - coefficient affecting the threshold value
     */
     void setK(double k) {
         k_ = k;
     }
 
     /**
-     * @brief Сеттер для target_row
-     * @param[in] target_row - ряд, для которого будет осуществляться визуализация
+     * @brief target_row setter
+     * @param[in] target_row - the row for which the visualization will be performed
     */
     void setTargetRow(int target_row) {
         target_row_ = target_row;
     }
 
     /**
-     * @brief Сеттер для image
-     * @param[in] image - входное изображение
+     * @brief image setter
+     * @param[in] image - input image
     */
     void setInputImage(const cv::Mat& image) {
         image_ = image.clone();
     }
 
 private:
-    cv::Mat image_; /*image_ - входное изображение */
-    int window_size_ = 1; /*window_size_ - азмер области, в котором будет осуществляться бинаризация */
-    double k_ = 0.2; /*k - коэффициент, влияющий на величину порогового значения*/
-    int target_row_ = 1; /*target_row - ряд, для которого будет осуществляться визуализация*/
-    double scale_ = 1.0; /*scale - параметр масштабирования графика (по умолчанию = 1)*/
+    cv::Mat image_; /*image_ - input image */
+    int window_size_ = 1; /*window_size_, determining the window size for the algorithm */
+    double k_ = 0.2; /*k - coefficient affecting the threshold value*/
+    int target_row_ = 1; /*target_row - the row for which the visualization will be performed*/
+    double scale_ = 1.0; /*scale - the coefficient with which the graph will be scaled (default = 1)*/
 };
 
 #endif NIBLACKBINARIZATION_H
