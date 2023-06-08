95337103629d2c1a817d92aab4cc5058c68a0547
Nikolai Shestakov <nick_moran@mail.ru>
2023-06-08 16:59:49 +0300

    delete some cpp-files, resolve conflicts

--- shestakov_n_a/prj.lab/matrixs/include/matrixs/matrixs.hpp
+++ shestakov_n_a/prj.lab/matrixs/include/matrixs/matrixs.hpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #pragma once
 #ifndef MATRIXS_MATRIXS_HPP_20230317
 #define MATRIXS_MATRIXS_HPP_20230317
@@ -33,40 +32,3 @@ private:
 };
 
 #endif
-||||||| empty tree
-=======
-#pragma once
-#ifndef MATRIXS_MATRIXS_HPP_20230317
-#define MATRIXS_MATRIXS_HPP_20230317
-
-#include <sstream>
-#include <vector>
-
-class MatrixS {
-public:
-    using SizeType = std::tuple<std::ptrdiff_t, std::ptrdiff_t>;
-public:
-    explicit MatrixS(const SizeType& size = { 0, 0 });
-    MatrixS(const std::ptrdiff_t row_count, const std::ptrdiff_t col_count);
-    ~MatrixS();
-    MatrixS& operator=(const MatrixS& rhs);
-    MatrixS(const MatrixS& mat);
-
-    std::ptrdiff_t nRows() const noexcept;
-    std::ptrdiff_t nCols() const noexcept;
-    const SizeType& ssize() const noexcept;
-    int32_t& at(const SizeType& elem);
-    const int32_t& at(const SizeType& elem) const;
-    int32_t& at(const std::ptrdiff_t i_row, const std::ptrdiff_t j_col);
-    const int32_t& at(const std::ptrdiff_t i_row, const std::ptrdiff_t j_col) const;
-
-    void resize(const SizeType& new_size);
-    void resize(const std::ptrdiff_t new_i, const std::ptrdiff_t new_j);
-private:
-    std::ptrdiff_t n_row_{ 0 };
-    std::ptrdiff_t n_col_{ 0 };
-    int32_t* data_{ nullptr };
-};
-
-#endif
->>>>>>> d41395efa836f5c7d767c2452853c96d33f87cda
--- shestakov_n_a/prj.test/geo2d_test.cpp
+++ shestakov_n_a/prj.test/geo2d_test.cpp
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
 
 #include <geo2d/geo2d.hpp>
-||||||| empty tree
-=======
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include <doctest/doctest.h>
-
-#include <geo2d/geo2d.hpp>
->>>>>>> d41395efa836f5c7d767c2452853c96d33f87cda
